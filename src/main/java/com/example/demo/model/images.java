db52b3dc584f78b8b396ba23be194d4dffcdb29d
anhnguyen888 <nd.anh@hutech.edu.vn>
2024-06-05 14:02:59 +0700

    Create images.java

---
+++ src/main/java/com/example/demo/model/images.java
@@ -0,0 +1,23 @@
+package com.example.buoi3lt.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "images")
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "image")
+    private String image;
+    private boolean isView;
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+}
